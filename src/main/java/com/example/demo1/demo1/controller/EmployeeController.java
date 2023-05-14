42a6a7ce0218c30ba26112da7dcec9093dc4f8dc
AKSHAY G83 <akshayghanekar83@gmail.com>
2023-05-14 20:58:23 +0530

    crud operations

--- src/main/java/com/example/demo1/demo1/controller/EmployeeController.java
+++ src/main/java/com/example/demo1/demo1/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.example.demo1.demo1.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo1.demo1.pojo.Employee;
@@ -23,9 +27,24 @@ public class EmployeeController{
         return employeeService.insertEmployee(emp);
     }
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello";
+    @PostMapping("/getAllEmployee")
+    public List<Employee> getAllEmployee(){
+        return employeeService.getAllEmployee();
+    }
+
+    @PutMapping("/updateEmployee")
+    public Employee updateEmployee(@RequestBody Employee emp){
+        return employeeService.updateEmployee(emp);
+    }
+
+    @GetMapping("/getEmployee/{empId}")
+    public Employee findEmployee(@PathVariable("empId")String empId){
+        return employeeService.findEmployee(empId);
+    }
+
+    @DeleteMapping("/deleteEmployee/{empId}")
+    public Employee deleteEmployee(@PathVariable("empId")String empId){
+        return employeeService.deleteEmployee(empId);
     }
 
 }
--- src/main/java/com/example/demo1/demo1/service/EmployeeService.java
+++ src/main/java/com/example/demo1/demo1/service/EmployeeService.java
@@ -1,7 +1,12 @@
 package com.example.demo1.demo1.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo1.demo1.pojo.Employee;
 import com.example.demo1.demo1.repository.EmployeeRepository;
@@ -14,6 +19,27 @@ public class EmployeeService {
     
     public Employee insertEmployee(Employee emp){
         employeeRepository.save(emp);
+        return emp;
+    }
+
+    public List<Employee> getAllEmployee(){
+        List<Employee> empList = new ArrayList<Employee>();
+        employeeRepository.findAll().iterator().forEachRemaining(empList::add);
+        return empList;
+    }
+
+    public Employee updateEmployee(Employee emp){
+        employeeRepository.save(emp);
+        return emp;
+    }
+
+    public Employee findEmployee(String empId){
+        return employeeRepository.findById(empId).get();
+    }
+
+    public Employee deleteEmployee(String empId){
+        Employee emp = findEmployee(empId);
+        employeeRepository.deleteById(empId);
         return emp;
     }
 
